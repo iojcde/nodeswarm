504ecf32751bdea0067bc2de2d2470edfab4710c
JcdeA <jcde@jcde.xyz>
2020-10-17 17:08:27 +0900

    blah blah blah 3

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var os = require('os');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`<h1>I am ${hostname}</h1>`);
+    res.end(`<h1>I am ${os.hostname()}</h1>`);
 }).listen(8080);
