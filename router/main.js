95b831d5c05fa041d623f238809292a74e979638
JcdeA <jcde@jcde.xyz>
2020-11-01 08:53:15 +0900

    remove unneccecery files

--- router/main.js
+++ router/main.js
@@ -1,16 +1,8 @@
 module.exports = function(app)
 {
    app.get('/',function(req,res){
-      res.render('dotOS.html')
+      res.render('index.html')
    });
-
-   app.get('/fridge',function(req,res){
-      res.render('fridge.html')
-   });
-
-
-   app.get('/dt',function(req,res){
-      res.render('dotOS.html')
 
    app.get('*', function (req, res) { 
       res.sendFile(__dirname+'/public/dotOS.html'); 
