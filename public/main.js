1204a9ebdefc00918e1da4a8d2337a9bacc9bb0d
JcdeA <jcde@jcde.xyz>
2020-11-01 16:36:07 +0900

    Remove personal files

--- public/main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    $("html").fadeIn(500);
-   
-    
-    
-});
--- public/resources/index2.js
+++
@@ -1,8 +0,0 @@
-var addButtonTrigger = function addButtonTrigger(el) {
-	el.addEventListener('click', function () {
-		var popupEl = document.querySelector('.' + el.dataset.for);
-		popupEl.classList.toggle('popup--visible');
-	});
-};
-
-Array.from(document.querySelectorAll('button[data-for]')).forEach(addButtonTrigger);
