64cb93f42ba70b8713718af0fbe084770c7adab9
JcdeA <jcde@jcde.xyz>
2020-11-01 16:33:33 +0900

    remove unneccecery files

--- router/main.js
+++ router/main.js
@@ -5,7 +5,7 @@ module.exports = function(app)
    });
 
    app.get('*', function (req, res) { 
-      res.sendFile(__dirname+'/public/dotOS.html'); 
+      res.sendFile(__dirname+'/public/index.html'); 
    }) 
      });
 
